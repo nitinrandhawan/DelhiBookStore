be5a466f99f8b33a794950528c9c709a1a578151
nitinrandhawan <nitinrandhawan@gmail.com>
2025-06-05 18:31:44 +0530

    allowed image in next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com","localhost","picsum.photos"],  
+    domains: ["res.cloudinary.com","localhost","picsum.photos","api.wwseals.com"],  
   },
 };
 
